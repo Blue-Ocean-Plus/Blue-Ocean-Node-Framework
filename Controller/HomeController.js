1d9283001490098c1e3ccd122c67d23b8d6f4bbc
amir hossein habibi <habibi.dev@gmail.com>
2020-09-06 20:35:22 +0430

    Create HomeController.js

---
+++ Controller/HomeController.js
@@ -0,0 +1,10 @@
+const {compact} = require("../Functions/template");
+const {siteName} = require("../Config/app");
+
+
+const home = (req, res) => {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(compact('./View/index.html', {'title': siteName}));
+};
+
+module.exports = {home};
