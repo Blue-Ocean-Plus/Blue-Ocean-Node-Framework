9b81c5b9745c67d5ce79b404352e666c1fb8d4ab
amir hossein habibi <habibi.dev@gmail.com>
2020-09-06 22:31:18 +0430

    Update HomeController.js

--- Controller/HomeController.js
+++ Controller/HomeController.js
@@ -5,6 +5,7 @@ const {siteName} = require("../Config/app");
 const home = (req, res) => {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.write(compact('./View/index.html', {'title': siteName}));
+    res.end();
 };
 
 module.exports = {home};
