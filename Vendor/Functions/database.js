45202a7d973c41ed06e27e34c6663572b783c1a9
amir hossein habibi <habibi.dev@gmail.com>
2020-09-13 22:48:59 +0430

    Update database.js

--- Vendor/Functions/database.js
+++ Vendor/Functions/database.js
@@ -1,41 +1,23 @@
-const {dbName} = require("../../Config/database");
-const mongodb = require('mongodb');
+const {MongoClient} = require("mongodb");
+
 
 module.exports = {
     DBInsert: (data, collectionName, qUpdate) => {
-        mongodb.MongoClient.connect("mongodb://localhost:27017/", function (err, db) {
-            if (err) return console.log("connect mongodb failed");
-
-            let dbm = db.db(dbName);
-
-            dbm.collection(collectionName).findOne(qUpdate, function (err, result) {
-                if (err) return console.log("Find failed");
-
-                if (result) {
-
-                    data.updated_at = data.date;
-                    delete data.date;
-                    let update = {$set: data};
-
-                    dbm.collection(collectionName).updateOne(qUpdate, update, function (err, res) {
-                        if (err) return console.log(err);
-                        console.log("1 document updated");
-                        db.close();
-                    });
-                } else {
-                    dbm.collection(collectionName).insertOne(data, function (err, res) {
-                        if (err) return console.log(err);
-                        console.log("1 document inserted");
-                        db.close();
-                    });
-                }
-
-
-            });
-
-        });
     },
-    DBFind:()=>{
+    DBFindOne: async (conn = "mongodb://localhost:27017/", dbName, collectionName, where = {}, option = {}) => {
+        const client = new MongoClient(conn);
 
+        await client.connect();
+
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
+
+        const result = await collection.findOne(where, option);
+
+        // const response = result.toArray();
+
+        await client.close();
+
+        return result;
     }
 };
